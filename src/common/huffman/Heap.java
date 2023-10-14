aac94830351905a8f216519c469975b33729a6df
mateus_lanuce <mateuslanuce@gmail.com>
2023-10-13 21:21:33 -0300

    atualização dos comentarios

--- src/common/huffman/Heap.java
+++ src/common/huffman/Heap.java
@@ -1,35 +1,38 @@
 package common.huffman;
 
 public class Heap {
-        private Node[] heap;
-        private int size = 0;
-        
+        private Node[] heap;  // Array para armazenar os elementos do heap
+        private int size = 0; // Tamanho atual do heap
+
         public int getSize() {
                 return size;
         }
 
-        public Heap(int i){
-                 heap = new Node[i];
+        public Heap(int i) {
+                heap = new Node[i]; // Inicializa o array do heap com o tamanho especificado
         }
-                
+
+        // Insere um nó no heap
         public void insert(Node k) {
                 size++;
                 int i = size;
-                while(i > 1 && heap[parent(i)].getFreq() > k.getFreq()) {
+                while (i > 1 && heap[parent(i)].getFreq() > k.getFreq()) {
                         heap[i] = heap[parent(i)];
                         i = parent(i);
                 }
                 heap[i] = k;
         }
 
-        public Node getMin(){
-                if(size != 0)
+        // Obtém o nó com o valor mínimo no heap
+        public Node getMin() {
+                if (size != 0)
                         return heap[1];
                 return null;
         }
-        
+
+        // Remove e retorna o nó com o valor mínimo no heap
         public Node delMin() {
-                if(size != 0) {
+                if (size != 0) {
                         Node min = heap[1];
                         heap[1] = heap[size];
                         size--;
@@ -38,45 +41,50 @@ public class Heap {
                 }
                 return null;
         }
-        
+
         /**
-         * Maintains the Minimum Heap Property A[PARENT (i)] <= A[i]
-         * @param i root of tree
+         * Mantém a propriedade do Heap Mínimo: A[PARENT(i)] <= A[i]
+         * @param i Raiz da árvore a ser heapificada
          */
         public void heapify(int i) {
                 int l = left(i);
                 int r = right(i);
                 int smallest;
-                if(r <= size) {
-                        if(heap[l].getFreq() < heap[r].getFreq())
+
+                if (r <= size) {
+                        if (heap[l].getFreq() < heap[r].getFreq())
                                 smallest = l;
                         else
                                 smallest = r;
-                        if(heap[i].getFreq() > heap[smallest].getFreq()) {
+
+                        if (heap[i].getFreq() > heap[smallest].getFreq()) {
                                 swap(i, smallest);
                                 heapify(smallest);
                         }
-                }
-                else if(l == size && heap[i].getFreq() > heap[l].getFreq()) {
+                } else if (l == size && heap[i].getFreq() > heap[l].getFreq()) {
                         swap(i, l);
-                }               
+                }
         }
 
+        // Troca dois elementos no heap
         private void swap(int i, int l) {
                 Node tmp = heap[i];
                 heap[i] = heap[l];
                 heap[l] = tmp;
         }
-        
+
+        // Retorna o índice do pai de um elemento no heap
         public int parent(int i) {
-                return i/2;
+                return i / 2;
         }
-        
+
+        // Retorna o índice do filho esquerdo de um elemento no heap
         public int left(int i) {
-                return 2*i;
+                return 2 * i;
         }
-        
+
+        // Retorna o índice do filho direito de um elemento no heap
         public int right(int i) {
-                return 2*i+1;
+                return 2 * i + 1;
         }
 }
